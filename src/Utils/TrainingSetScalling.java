47256f621c65444a1bf88d544ce3146e4d9c13f1
Michal <pierogmichal@gmail.com>
2012-06-01 20:47:36 +0200

    Pierwsza czesc sprawka skonczona, zabieram sie za wnioski

--- src/Utils/TrainingSetScalling.java
+++ src/Utils/TrainingSetScalling.java
@@ -1,8 +1,10 @@
 package Utils;
 
-import java.awt.*;
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.awt.image.renderable.ParameterBlock;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
@@ -17,7 +19,6 @@ import javax.media.jai.KernelJAI;
 import javax.media.jai.PlanarImage;
 import javax.media.jai.RenderedOp;
 import javax.media.jai.operator.BinarizeDescriptor;
-import javax.media.jai.operator.CropDescriptor;
 import javax.media.jai.operator.MedianFilterDescriptor;
 
 /**
@@ -25,9 +26,7 @@ import javax.media.jai.operator.MedianFilterDescriptor;
  * @author Michal
  */
 public class TrainingSetScalling {
-    private static final int margin=15;
-    private static final int WIDTH =(640-2*margin)/6, HEIGHT=(480-2*margin)/6;
-    
+
     public static void main(String[] args) {
         new TrainingSetScalling().resizeAll(Config.dataPath);
     }
@@ -92,20 +91,8 @@ public class TrainingSetScalling {
                     KernelJAI kernel2 = new KernelJAI(3,3,kernelMatrix2);
                     PlanarImage im1 =JAI.create("gradientmagnitude", op,kernel, kernel2);
                     
-                    
-                    
-                    //PlanarImage convolved = JAI.create("convolve", op, kernel);
-                   // PlanarImage convolved2 = JAI.create("convolve", op, kernel2);
-                    
-                         // Create the ParameterBlock for the operation
-                    //ParameterBlock pb = new ParameterBlock();
-                   // pb.addSource(convolved);
-                 //   pb.addSource(convolved2);
-
-                    // Create the Add operation.
-                   // PlanarImage dst = JAI.create("add", pb);
                     PlanarImage binarized = BinarizeDescriptor.create(im1, 60.0, null);
-                    kernelMatrix = new  float[]{0,1,0,
+                    kernelMatrix2 = new  float[]{0,1,0,
                                                 1,1,1,
                                                 0,1,0};
                     kernel2 = new KernelJAI(3,3,kernelMatrix2);
@@ -113,18 +100,14 @@ public class TrainingSetScalling {
                     binarized = JAI.create("erode", binarized,kernel2);
                     
                     Rectangle  rect = getFaceBounds(binarized.getAsBufferedImage());
-                    
-                    //op = CropDescriptor.create(op,(float)rect.x,(float)rect.y,(float)rect.width,(float)rect.height, null);
-                    
-                    float diff = 480/(float)rect.width;
-                    
+
+                    float diff = 480/(float)rect.width;                    
                     int prefW=480,  prefH=560;
                     
                     padded = new BufferedImage(prefW ,(int)(rect.height*diff), BufferedImage.TYPE_BYTE_GRAY);
                     g = padded.createGraphics();
                     //480x600
-                    
-                    
+              
                     g.setComposite(AlphaComposite.Src);
                     g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
                     g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
@@ -214,108 +197,3 @@ public class TrainingSetScalling {
         return rect;
     }
 }
-
-/*
- * 
- *     public void resizeAll(String source) {
-        File mainFolder = new File(source);
-        File subFolders[] = mainFolder.listFiles(new FileFilter() {
-
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.isDirectory();
-            }
-        });
-
-        FileFilter jpgFilter = new FileFilter() {
-
-            @Override
-            public boolean accept(File pathname) {
-                String name = pathname.getName();
-                int i = name.lastIndexOf(".");
-                if (i == -1) {
-                    return false;
-                }
-                return name.substring(i + 1).equalsIgnoreCase("jpg");
-            }
-        };
-
-        File images[];
-        BufferedImage padded = null;
-        Graphics2D g = null;
-
-        Iterator iter = ImageIO.getImageWritersByFormatName("jpeg");
-        ImageWriter writer = (ImageWriter) iter.next();
-        ImageWriteParam iwp = writer.getDefaultWriteParam();
-        iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        iwp.setCompressionQuality(1);
-
-        FileImageOutputStream outStream;
-
-        for (File folder : subFolders) {
-            images = folder.listFiles(jpgFilter);
-
-
-
-            for (File image : images) {
-                System.out.println("Resizing " + image.getName());
-                try {
-                    BufferedImage img = ImageIO.read(image); 
-
-                    RenderedOp op = MedianFilterDescriptor.create(img, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 3, null);
-                //    op = MedianFilterDescriptor.create(op, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 3, null);
-                 //   op = MedianFilterDescriptor.create(op, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 5, null);
-                 //   op = MedianFilterDescriptor.create(op, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 5, null);
-                 //   RenderedOp op2 = MedianFilterDescriptor.create(op, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 12, null);
-//                    op2 = MedianFilterDescriptor.create(op2, MedianFilterDescriptor.MEDIAN_MASK_SQUARE, 7, null);
-//                    
-//                    
-//                    float[] kernelMatrix = {    -1, -2, -1,
-//                                                -2, 12, -2,
-//                                                -1, -2, -1 };
-//                    KernelJAI kernel = new KernelJAI(3,3,kernelMatrix);
-//                    PlanarImage convolved = JAI.create("convolve", op2, kernel);
-//                   
-//                    PlanarImage binarized = BinarizeDescriptor.create(convolved, 120.0, null);
-                    
-                    
-                 //   Rectangle  rect = getFaceBounds(binarized.getAsBufferedImage());
-                   // processed = JAI.create("convolve", processed, kernel);
-                    padded = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_BYTE_GRAY);
-                    g = padded.createGraphics();
-                    
-                    
-                    g.setComposite(AlphaComposite.Src);
-                    g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-                    g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-                    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                    g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-                    g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-                 //   int w = (480-rect.width)/2;
-                 //   g.setColor(new Color(153,153,153));
-                    
-                 //   BufferedImage pad= op.getAsBufferedImage().getSubimage(rect.x, rect.y, rect.width, rect.height);
-                //    g.fillRect(0,0,pad.getWidth()+2*w,padded.getHeight());
-
-               //     g.drawImage(pad,w,padded.getHeight()-pad.getHeight(),null);
-                    
-                    
-                    BufferedImage copy = op.getAsBufferedImage().getSubimage(margin,margin,op.getWidth()-margin*2,op.getHeight()-margin*2);
-                    g.drawImage(copy, 0, 0, WIDTH,HEIGHT,null);
-                    g.dispose();
-
-                    outStream = new FileImageOutputStream(image);
-                    writer.setOutput(outStream);
-                    IIOImage outImg = new IIOImage(padded, null, null);
-                    writer.write(null, outImg, iwp);
-                    outStream.close();
-                    writer.reset();
-
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex.getMessage());
-                }
-            }
-
-        }
-    }
- */
